908a2c5df884279b8f71206fe72414a97520d709
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-09-26 17:35:02 +0700

    คิวรี่ข้อมูลด้วย where แบบง่าย
    คิวรี่ข้อมูลด้วย where แบบง่าย

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -5,17 +5,36 @@ const App = () => {
 };
 
 /* 
-ลบเอกสารจากคอลเล็กชั่น
-ถ้าต้องการลบเอกสารจากคอลเล็กชั่น จะใช้เมธอด delete() ซึ้งผลลัพธ์จากการเรียกใช้เมธอดคือ Promise ต่อไปนี้เป็นตัวอย่างการลบเอกสารด้วยเมธอด delete()
+คิวรีข้อมูลแบบง่ายๆ 
+การคิวรีข้อมูลใน firestore จะใช้เมธอด where() โดยผ่านพารามิเตอร์ เพื่อกำหนดเงื่อนไขของการคิวรี่ ซึ้งรุปแบบพื้นฐานของเมธอด where จะเป็นดังนี้
+รูปแบบ
+where(fieldName, queryOperator, value)
+-where คือเมธอดที่ใช้กำหนดคิวรี่ เพื่อเลือกเฉพาะเอกสารที่ตรงกับเงื่อนไขมาจากคอลเล็กชัน ผลลัพธ์ที่ได้จากเมธอด where() จะเป็น query ออบเจ็กต์ ที่ใช้อ้างอิงไปยังเอกสารต่างๆ ที่ตรงกับเงื่อนไข
+-fieldName คือชื่อฟิลด์ที่ใช้เป็นเงื่อนไขในการคิวรี่ข้อมูลจากฐานข้อมูล
+-queryOperator คือ โอเปอร์เรตอร์ที่ใช้บอกขอบเขตของเงื่อนไข ซึ้งโอเปอร์เรเตอร์ที่สามารถใช้กับเมธอด where() ได้แก่ <, <=, ==, >, >=, array-contains, in และ array-contains-any
+-value คือ ค่าที่ใช้ตรวจสอบ
 
-const docRef = firestore.doc("users/id");
-docRef
-.delete()
-.then(() => {
-  consol.log("Document Deleted!!!");
+ต่อไปนี้เป็นตัวอย่างการกำหนดคิวรี เพื่อเลือกเอกสารมาจาคอลเล็กชั่น users
+
+const docRef = firestore.collection("users");
+const query1 = docRef.where("userName", "==", "โรนัลโด้")
+const query2 = docRef.where("age", ">=", 25)
+
+ผลลัพธ์ที่ได้จากเมธอด where() คือ query ออบเจ็กต์ที่ได้อ้างอิงไปยังเอกสารต่างๆ ที่ตรงกับเงื่อนไข ไม่ใช้ข้อมูลเอกสาร ดังนั้นหากต้องการนำเอกสารมาใช้งาน จะต้องเรียกเมธอด get() อีกครั้งหนึ่ง
+
+const docRef = firestore.collection("users")
+const query = docRef.where("userName", "==", "โรนัลโด้")
+query
+.get()
+.then((querySnapshot)=>{
+  querySnapshot.forEach((doc)=>{
+    if(doc.exists){
+        console.log(doc);
+    }
+  })
 })
-.catch((error) => {
-  console.log("Error getting document:", error);
+.catch((err) => {
+  console.log(err);
 })
 */
 
