44f875c117fca429a90dcd58ecdad873fb325f4c
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-09-28 23:39:27 +0700

    การล๊อกเอาต์ออกจากระบบ

--- firebase/src/SignIn.js
+++ firebase/src/SignIn.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { auth, googleProvider } from "./database/firebase";
+/* 
+การล๊อกเอาต์ออกจากระบบ
+หลังจากล๊อกอินด้วย Google แล้วหากต้องการล็อกเอาต์ออกจากระบบ จะต้องเรียกใช้งานเมธอด auth.signOut() 
+
+*/
 export default function SignIn() {
   const googleLoginHandler = async () => {
     const userCredential = await auth.signInWithPopup(googleProvider);
     console.log(userCredential.user);
   };
+
+  const logOut = () => {
+      auth.signOut()
+      .then(() => {
+          console.log("Logout Out")
+      })
+      .catch((err) => {
+          console.log("LOgout Not Ok." + err);
+      })
+  }
 
   return (
     <div>
@@ -12,3 +27,4 @@ export default function SignIn() {
     </div>
   );
 }
+
