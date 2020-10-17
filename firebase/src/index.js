3bc9a7ca74b9bdd98be8971397c815ef266a5db3
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-17 16:34:26 +0700

    กำหนดกฏด้วยการอ้างอิงค่าในฟิลด์ต่างๆ ของเอกสาร
    กำหนดกฏด้วยการอ้างอิงค่าในฟิลด์ต่างๆ ของเอกสาร

--- firebase/src/index.js
+++ firebase/src/index.js
@@ -18,27 +18,22 @@ serviceWorker.unregister();
 
 /* 
 
-การพิจารณาเมื่อมีกฏหลายชุด
-service cloud.firestore {
+กำหนดกฏด้วยการอ้างอิงค่าในฟิลด์ต่างๆ ของเอกสาร
+
+วิธีอ้างอิงค่าซึ้งเก็บไว้ในฟิลด์ต่างๆ ของเอกสารในคอลเล็กชันปัจจุบัน จะใช้คำสั่ง resource.data ตามด้วยชื่อฟิลด์ที่ต้องการตรวจสอบค่า เช่น resource.data.displayName หมายถึง การอ้างอิงค่าที่อยู่ในฟิลด์ displayName
+
+ต่อไปนี้เป็นตัวอย่างการกำหนดกฏ ตรวจสอบว่าโพสต์การอ่านและแก้ไขเอกสารในคอลเล็กชัน posts โดยจะตรวจสอบก่อนว่า หากฟิลด์ visibility เก็บค่า public เอาไว้ก็จะอณุญาตให้อ่าน หรือแก้ไขเยื้อหาในโพสต์นั้นๆ ได้
+
+service cloud.firestore{
   match /databases/{database}/documents {
-    match /users/{userId}/{document=**} {
-      allow read, update, delete: if request.auth.uid == userId;
-      allow create: if request.auth.uid != null;
-    }
-    match /users/{userId}{
-      allow read, write: if true;
+    match /posts/{postId} {
+      allow read, write: if resource.data.visibility == 'public';
     }
   }
 }
 
-- กำหนดพาธไปยังคอลเล็กชัน users โดย {userId} คือตัวแปรที่เก็บค่า id ของเอกสารที่อยู่ในคอลเล็กชัน users โดยจะรวมถึงเอกสารต่างๆ ที่ อยู่ในคอลเล็กชันย่อยที่อยู่ภายใต้คอลเล็กชัน user ด้วย
-
-- อนุญาติให้ read, update หรือ delete หากผู้แก้ไขเป็นเจ้าของไฟล์
-- อณุญาติให้ create เฉพาะผู้ใช้ล๊อกอิน
-- กำหนดพาธไปยังคอลเล็กชัน users
-- อณุญาติให้ read หรือ write เอกสารได้
-
-แม้ว่าจะไม่ผ่านกฏบางข้อ แต่หากผ่านกฏเพียงกฏใดกฏหนึ่งก็สามารถจัดการกับเอกสารได้
+- กำหนดพาธไปเอกสารที่อยู่คอลเล็กชัน posts
+- กำหนดกฏ โดยอนุญาติให้ read และ write เมื่อข้อมูลที่อยู่ในฟิลด์ visibility มีค่าเป็น public
 
 
 
