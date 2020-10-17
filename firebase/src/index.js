c219a5f7ed260938710c9710a7b7f2cc9bfc4ddb
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-17 17:53:36 +0700

    การกำหนดฟังกืชัน security rules
    การกำหนดฟังกืชัน security rules

--- firebase/src/index.js
+++ firebase/src/index.js
@@ -18,33 +18,24 @@ serviceWorker.unregister();
 
 /* 
 
-การกำหนดกฏโดยอ้างอิงข้อมูลจากส่วนอื่นๆ ของฐานข้อมูล
-
-การอ้างอิงเอกสารที่อยู่ในคอลเล็กชันอื่นๆ ของ cloud firestore สามารถทำได้ 2 แบบ ดังนี้
-- แบบที่ 1 ใช้ get(path) เพื่อใช้ข้อมูลที่อยู่ในเอกสาร โดยระบุฟิลด์ที่ต้องการข้อมูล
-- แบบที่ 2 ใช้ exists(path) ใช้เพื่อตรวจสอบว่าเอกสารต้องการอ้างอิงถึงนั้นมีอยู่หรือไม่
-
-การอ้างอิงข้อมูลจากการใช้ get() หรือ exites() นั้น เราจะต้องผ่านค่าพาธที่ต้องการอ้างอิงเข้าไป ข้อสังเกตคือ การอ้างอิงตัวแปรใน get() หรือ exites() นั้นจะมีรูปแบบต่างจากการอ้างอิงพาธในส่วนของ match โดยจะไม่ใช้ {} ครอบชื่อตัวแปร แต่จะใช้ $() ครอบชื่อตัวแปรแทน เช่น $(userId), $(request.auth.id) เป็นต้น
-
-get(/databases/$(database)/documents/users/$(request.auth.uid)).data.role
-exists(/database/$(database)/documents/posts/$(request.resource.id)).data.email
-- เข้าไปยังคอลเล็กชัน users โดยเลือกเอกสารที่มีค่า id เท่ากับ request.auth.uid จากนั้นนำข้อมูลที่อยู่ในฟิลด์ role ของเอกสารมาใช้งาน
-- เข้าไปยังคอลเล็กชัน posts โดยเลือกเอกสารที่มีค่า id เท่ากับ request.resource.id แล้วตรวจสอบว่า ข้อมูลที่อยู่ในฟิลด์ email มีอยู่หรือไม่ ถ้ามีอยู่จะได้ผลลัพธ์เป็น true แต่ถ้าไม่มีอยู่ก็จะได้ผลลัพธ์เป็น false
-
-ต่อไปนี้เป็นตัวอย่างกฏ ซึ้งจะอณุญาตให้ write เอกสารได้เฉพาะผู้ใช้ที่มีฐานะเป็น admin เท่านั้นส่วนการ read จะอณุญาติให้กับผู้ใช้ที่ล๊อกอินเข้าสู่ระบบ
+การกำหนดฟังกืชัน security rules
+หากเงื่อนไขที่กำหนดใน security rules มีความซับซ้อน เราสามารถสร้างฟังก์ชันเพื่อทำหน้าที่ตรวจสอบกฏ โดยรีเทิร์นผลลัพธ์มาเป็น true หรือ false 
+ต่อไปนี้เป็นตัวอย่างฟังก์ชัน isAdmin() สำหรับตรวจสอบว่า ผู้ใช้มีฐานะเป็น admin และกำหนดฟังก์ชัน isLogin() เพื่อตรวจสอบว่าผู้ใช้ได้ล็อกอินเข้าสู่ระบบหรือไม่
 
 service cloud.firestore {
   match /databases/{database}/documents {
-    match /posts/{postId} {
-      allow write: if get(/databases/$(database)/documents/users/$(request.auth.uid)).data.role == 'admin';
-      allow read: if request.auth.uid != null;
+    match /users/{userId}/{document=**} {
+      function isAdmin() {
+        return get(/databases/$(database)/documents/users/$(request.auth.uid)).data.role == 'admin'
+      }
+      function isLogin() {
+        return request.auth.uid != null
+      }
+      allow read, update, delete: if isAdmin() && request.auth.uid == userId;
+      allow create: if isLogin();
     }
   }
 }
-
-- เข้าไปยังคอลเล็กชัน posts ไปยังเอกสารที่มีค่า id เท่ากับ postId 
-- ตรวจสอบเอกสาร โดยไปยังคอลเล็กชัน users และเลือกผู้ใช้ที่มีค่า id เท่ากับ request.auth.uid จากนั้นให้ดูข้อมูลที่อยู่ในฟิลด์ role หากมีค่าเท่ากับ admin แสดงว่าอณุญาติให้ write ได้
-- อณุญาติให้ read หากผู้ใช้ล๊อกอินเข้าสู่ระบบเรียบร้อยแล้ว
 
 
 
