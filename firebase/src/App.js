9766a4253736048617c7c5821f9a1a68b774384b
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-19 02:40:11 +0700

    อัปโหลดไฟล์อย่างง่าย
    อัปโหลดไฟล์อย่างง่าย

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -1,12 +1,70 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import LoginForm from "./components/LoginForm";
+import { storage } from "./database/firebase";
 
+/* อัปโหลดไฟล์อย่างง่าย */
 const App = () => {
+  const [file, setFile] = useState("");
+  const [link, setLink] = useState("");
+  const subBucketRef = storage.child("mainBucket/subBucket");
+  const uploadHandler = (e) => {
+    e.preventDefault();
+    if (!!file) {
+      const fileName = file.name;
+      const targetRef = subBucketRef.child(fileName);
+      targetRef.put(file).then((response) => {
+        console.log(response);
+        response.ref.getDownloadURL().then((photoURL) => {
+          console.log(photoURL);
+          setLink(photoURL);
+        });
+      });
+    } else {
+      console.log("no file upload!!");
+    }
+  };
   return (
-    <div className="container">
-      <LoginForm />
+    <div className="container mt-4">
+      <form className="d-flex justify-content-center align-items-center">
+        <div
+          style={{
+            display: "block",
+            position: "relative",
+          }}
+        >
+          <input
+            type="file"
+            className="custom-file-input"
+            id="customFile"
+            onChange={(e) => {
+              const file = e.target.files[0];
+              setFile(file);
+              setLink("");
+            }}
+          />
+          <label className="custom-file-label" htmlFor="customFile">
+            {!file ? <div>เลือกรูปภาพ</div> : <div>{file.name}</div>}
+          </label>
+        </div>
+        <div className="ml-3">
+          <button
+            type="submit"
+            onClick={uploadHandler}
+            className="btn btn-success"
+          >
+            upload
+          </button>
+        </div>
+      </form>
+      {!!link ? (
+        <div className="text-center mt-4">
+          <hr />
+          <a className="text-secondary" href={link}>
+            คลิกไฟล์ {file.name}
+          </a>
+        </div>
+      ) : null}
     </div>
   );
 };
--- firebase/src/index.js
+++ firebase/src/index.js
@@ -15,22 +15,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-/* 
-
-อับโหลดไฟล์ด้วยเมธอด put
-การอัพโหลดเราจะต้องอ้างอิงไปยังตำแหน่งของไฟล์ที่ต้องการก่อน (ตำแหน่งโฟลเดอร์และชื่อไฟล์) จากนั้นจึงใช้เมธอด put() เพื่ออัพโหลด
-
-const fileRef = storage.child("mainBucket/subBucket/myfile.jpg");
-fileRef
-.put(file)
-.then((response) => {
-  console.log(response);
-})
-
--ประกาศตัวแปร fileRef เพื่ออ้างอิงไปยังไฟล์ชื่อว่า myfile.jpg ซึ้งหากไฟล์นี้ไม่มีอยู่ cloud storage ก็จะสร้างให้ใหม่โดยอัตโนมัติ
-
-put(file)
-
-
-*/
